49ed7b735a9e8fcd4f2819430ce05d1fa931bac0
Roshankumar02 <roshankumar020205@example.com>
2026-03-15 23:28:57 +0530

    changed test_app.py file

--- test_app.py
+++ test_app.py
@@ -1,10 +1,7 @@
 import sys
 import os
-# testing
 sys.path.append(os.path.abspath(os.path.dirname(__file__)))
-
 from flask_todo.app import app
-
 def test_home_page():
     client = app.test_client()
     response = client.get("/")
