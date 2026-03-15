efa4ffd13da74287bd3cd11afb0d9239bb234547
Roshankumar02 <roshankumar020205@example.com>
2026-03-15 16:40:54 +0530

    fix deployment script

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,8 @@ echo "Starting deployment..."
 python3 -m venv deployenv
 source deployenv/bin/activate
 
+pip install -r requirements.txt
+
 pip install dist/*.whl
 
-python -m flask_todo.app
+python3 app.py
